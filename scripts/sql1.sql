6a81ffdc2c0e3be8af680bb51139103656026ab9
Rodrigo Monteiro Barboza <rodboza@gmail.com>
2018-01-19 09:19:50 +0000

    Update sql1.sql

--- scripts/sql1.sql
+++ scripts/sql1.sql
@@ -1,1 +1,21 @@
 --script SQL
+
+ALTER EVENT SESSION  AlwaysOn_health ON SERVER WITH (STARTUP_STATE=ON);
+GO
+
+CREATE LOGIN dbm_login WITH PASSWORD = 'P@ssw0rd';
+CREATE USER dbm_user FOR LOGIN dbm_login;
+GO
+
+CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'P@ssw0rd';
+CREATE CERTIFICATE dbm_certificate WITH SUBJECT = 'dbm';
+BACKUP CERTIFICATE dbm_certificate
+   TO FILE = '/var/opt/mssql/data/dbm_certificate.cer'
+   WITH PRIVATE KEY (
+           FILE = '/var/opt/mssql/data/dbm_certificate.pvk',
+           ENCRYPTION BY PASSWORD = 'P@ssw0rd'
+      );
+GO
+
+
+
