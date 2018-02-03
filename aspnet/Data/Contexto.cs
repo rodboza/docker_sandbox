26a5d923dd3b38e875afdd80159943f5cd02b244
Rodrigo Monteiro Barboza <rodboza@gmail.com>
2018-02-03 20:52:06 -0200

    Update Contexto.cs

--- aspnet/Data/Contexto.cs
+++ aspnet/Data/Contexto.cs
@@ -11,14 +11,18 @@ namespace aspnet.Data
     {
 
         public DbSet<Person> Persons { get; set; }
-        
-        public Contexto()
-        {
+	
+	public DbContextOptions<Contexto> Init ()
+	{
 		//string connectionString = @"Data Source=13.72.79.75;Initial Catalog=db1;Persist Security Info=True;User ID=sa;Password=P@ssw0rd";
 		string connectionString = @"Data Source=haproxy;Initial Catalog=db1;Persist Security Info=True;User ID=sa;Password=P@ssw0rd;ApplicationIntent=ReadOnly";
 		var options = new DbContextOptionsBuilder<Contexto>();
-		options.UseSqlServer(connectionString);
-		base(options.Options);        
+		return options.UseSqlServer(connectionString);
+	}
+        
+        public Contexto()
+		:this(Init())
+	{		
         }
 
         public Contexto(DbContextOptions<Contexto> options)
