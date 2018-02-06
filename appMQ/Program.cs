5f7b893512bf11dfa616cc7f6ca12f2de087de93
rodboza <rodboza@gmail.com>
2018-02-06 23:15:13 +0000

    Criação do projeto/container do Active MQ

---
+++ appMQ/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace appMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+string TOPIC_NAME = "PersonsTopic";
+//string BROKER = "tcp://activemq:61616";
+string BROKER = "tcp://localhost:61616";
+SimpleTopicPublisher publisher;
+publisher = new SimpleTopicPublisher(TOPIC_NAME, BROKER);
+publisher.SendMessage(@"{Id:111,FirtName:Rodrigo,MiddleName:M,LastName:Barboza}");
+publisher.SendMessage(@"{Id:222,FirtName:Rodrigo,MiddleName:M,LastName:Barboza}");
+publisher.SendMessage(@"{Id:333,FirtName:Rodrigo,MiddleName:M,LastName:Barboza}");
+publisher.Dispose();
+
+            Console.WriteLine("Mensagem Enviada!");
+
+string CLIENT_ID = "ActiveMqFirstSubscriber";
+string CONSUMER_ID = "ActiveMqFirstSubscriber";
+
+using (SimpleTopicSubscriber subscriber = new SimpleTopicSubscriber(TOPIC_NAME, BROKER, CLIENT_ID, CONSUMER_ID))
+{
+subscriber.OnMessageReceived += new MessageReceivedDelegate(subscriber_OnMessageReceived);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+}
+
+
+
+        }
+
+static void subscriber_OnMessageReceived(string message)
+{
+Console.WriteLine(message);
+}
+
+    }
+}
---
+++ appMQ/SimpleTopicPublisher.cs
@@ -0,0 +1,50 @@
+using System;
+using Apache.NMS.ActiveMQ;
+using Apache.NMS.ActiveMQ.Commands;
+using Apache.NMS;
+namespace appMQ
+{
+public class SimpleTopicPublisher : IDisposable
+{
+private readonly string topicName = null;
+private readonly IConnectionFactory connectionFactory;
+private readonly IConnection connection;
+private readonly ISession session;
+private readonly IMessageProducer producer;
+private bool isDisposed = false;
+public SimpleTopicPublisher(string topicName, string brokerUri)
+{
+this.topicName = topicName;
+this.connectionFactory = new ConnectionFactory(brokerUri);
+this.connection = this.connectionFactory.CreateConnection();
+this.connection.Start();
+this.session = connection.CreateSession();
+ActiveMQTopic topic = new ActiveMQTopic(topicName);
+this.producer = this.session.CreateProducer(topic);
+}
+public void SendMessage(string message)
+{
+if (!this.isDisposed)
+{
+ITextMessage textMessage = this.session.CreateTextMessage(message);
+this.producer.Send(textMessage);
+}
+else
+{
+throw new ObjectDisposedException(this.GetType().FullName);
+}
+}
+#region IDisposable Members
+public void Dispose()
+{
+if (!this.isDisposed)
+{
+this.producer.Dispose();
+this.session.Dispose();
+this.connection.Dispose();
+this.isDisposed = true;
+}
+}
+#endregion
+}
+}
