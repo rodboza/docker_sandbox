5a8791b04ff1f439c86b77d299e56937686aa4be
Rodrigo Monteiro Barboza <rodboza@gmail.com>
2018-06-16 15:32:37 -0300

    Update run.sh

--- dbmq/run.sh
+++ dbmq/run.sh
@@ -1,5 +1,5 @@
-docker stop -t0 dbmq
-docker rm dbmq
+docker stop -t0 dbmq1 dbmq2 dbmq3
+docker rm dbmq1 dbmq2 dbmq3
 docker build -t mariadb-netcore .
 docker run -d --network SQL -p 5001:80 --name dbmq1 mariadb-netcore
 docker run -d --network SQL -p 5002:80 --name dbmq2 mariadb-netcore
