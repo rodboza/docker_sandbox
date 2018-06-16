6878f37988eda621da4755f1b8e6187633303d94
rodboza <rodboza@gmail.com>
2018-06-16 10:03:32 +0000

    alteração da chamada do build

--- dbmq/run.sh
+++ dbmq/run.sh
@@ -1,6 +1,6 @@
 docker stop -t0 dbmq
 docker rm dbmq
-docker build -t mariadb-netcore -f suptst.DF .
+docker build --no-cache -t mariadb-netcore .
 docker run -d --network SQL -p 5000:80 --name dbmq mariadb-netcore
 
 
