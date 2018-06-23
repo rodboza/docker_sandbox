6975c2c5e20147f4b06710022c9df1e4b7f4d6a8
rodboza <rodboza@gmail.com>
2018-06-22 23:35:43 -0300

    Modificando o app web para acesso ao mysql

---
+++ aspnet/Controllers/MsgController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using aspnet.Models;
+using aspnet.Data;
+
+namespace aspnet.Controllers
+{
+    public class MsgController : Controller
+    {
+        private readonly IDataService _dataService;
+
+        public MsgController(IDataService dataService)
+        {
+            this._dataService = dataService;
+        }
+
+
+        public IActionResult Index()
+        {
+            List<Msg> msgs = _dataService.GetMsgs();
+            return View(msgs);
+        }
+    }
+}
---
+++ aspnet/Models/Msg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnet.Models
+{
+    public class Msg
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime data { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public String host { get; set; }
+        public String msg { get; set; }
+
+        public Msg()
+        {
+            
+        }
+
+        public Msg(string msg)
+            : this()
+        {
+            this.msg = msg;
+        }
+    }
+}
